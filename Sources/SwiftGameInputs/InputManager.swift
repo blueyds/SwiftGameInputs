b37373b82ca3beda3bcac3189ae51594f1dbb8ee
Blueyds <9055449+blueyds@users.noreply.github.com>
2025-01-11 14:50:11 -0600

    error

--- Sources/SwiftGameInputs/InputManager.swift
+++ Sources/SwiftGameInputs/InputManager.swift
@@ -86,7 +86,7 @@ extension InputManager{
 }
 // float4 extensions
 extension InputManager{
-	public func set(float4ID: Int, to value: SIMD4<Float>){
+	public func set(float4ID: Int, to value: SIMD4<Float>?){
 		lock()
 		if iFloat4s.updateValue(value, forKey: float4ID) == nil{
 			print("InputManager.set(float4Id: \(float4ID), to: \(value))  set a key that was not already set")
@@ -95,14 +95,14 @@ extension InputManager{
 	}
 	
 	public func get(float4ID: Int)->SIMD4<Float>?{
-	    if let result = iFloat4s[floatID]{ return result}
+	    if let result = iFloat4s[float4ID]{ return result}
 	    else { return nil}
 	}
 }
 
 // float8 extensions
 extension InputManager{
-	public func set(float8ID: Int, to value: SIMD8<Float>){
+	public func set(float8ID: Int, to value: SIMD8<Float>?){
 		lock()
 		if iFloat8s.updateValue(value, forKey: float8ID) == nil{
 			print("InputManager.set(float8Id: \(float8ID), to: \(value))  set a key that was not already set")
